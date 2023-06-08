ef28329f62ff127c6ae3b4689f3b0d65ada8c799
Smite Chow <xiaopengyou2no1@gmail.com>
2023-06-08 11:19:08 +0800

    implement get content info

--- src/xingtu/App.js
+++ src/xingtu/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import {getBasicInfo} from "./fetches";
+import {getBasicInfo, getContentInfo} from "./fetches";
 
 function getPriceInfo(prices) {
   return prices.map(item => {
@@ -11,16 +11,50 @@ function getPriceInfo(prices) {
   })
 }
 
+function getPercent(number, fixed=1) {
+  if (number) {
+    return `${(number*100).toFixed(fixed)}%`
+  }
+  return '-%'
+}
+
+function getLatest15Description(items) {
+  let play_min = 0
+  let play_max = 0
+  let famous = 0
+  let first = true;
+
+  for (let item of items) {
+    if (first) {
+      play_min = item.play
+      play_max = item.play
+      first = false
+    }
+    if (item.play < play_min) {
+      play_min = item.play
+    }
+    if (item.play > play_max) {
+      play_max = item.play
+    }
+    if (item.play > 1000*10000) {
+      famous ++
+    }
+  }
+
+  return [play_min, play_max, getPercent(famous/items.length)]
+}
+
 function App() {
   const [id, setId] = React.useState('')
-  const [result, setResult] = React.useState({})
+  const [basicInfo, setBasicInfo] = React.useState({})
+  const [contentInfo, setContentInfo] = React.useState({})
   return <><h1>星图数据</h1>
     <div className="basic">
       <h2>基本信息</h2>
       <input type="text" placeholder="请输入星图ID" value={id} onChange={event => setId(event.target.value)}/>
       <button onClick={async () => {
         let [baseInfo, marketingInfo, platformChannelInfo] = await getBasicInfo(id)
-        setResult({
+        setBasicInfo({
           nick_name: baseInfo.nick_name,
           tags_relation: Object.keys(baseInfo.tags_relation),
           douyin_pc_link: `https://www.douyin.com/user/${baseInfo.sec_uid}`,
@@ -28,9 +62,27 @@ function App() {
           price_info: getPriceInfo(marketingInfo.price_info.filter(item => item.enable && item.is_open && item.need_price && item.task_category == 1)),
           self_intro: platformChannelInfo.self_intro,
         })
-      }}>确定</button>
+      }}>获取</button>
       <div className="result">
-        {JSON.stringify(result)}
+        {JSON.stringify(basicInfo)}
+      </div>
+    </div>
+    <div className="content">
+      <h2>内容表现</h2>
+      <button onClick={async () => {
+        let [spreadInfo, showItems] = await getContentInfo(id)
+        setContentInfo({
+          share_avg: spreadInfo.share_avg,
+          comment_avg: spreadInfo.comment_avg,
+          like_avg: spreadInfo.like_avg,
+          play_mid: spreadInfo.play_mid,
+          interact_rate: getPercent(spreadInfo?.interact_rate?.value/10000),
+          play_over_rate: getPercent(spreadInfo?.play_over_rate?.value/10000),
+          latest_star_item_info: getLatest15Description(showItems.latest_star_item_info),
+        })
+      }}>获取</button>
+      <div className="result">
+        {JSON.stringify(contentInfo)}
       </div>
     </div>
   </>
--- src/xingtu/fetches.js
+++ src/xingtu/fetches.js
@@ -18,3 +18,19 @@ export async function getBasicInfo(id) {
     });
     return [await authorBaseInfo.json(), await authorMarketingInfo.json(), await authorPlatformChannelInfo.json()]
 }
+
+export async function getContentInfo(id) {
+    let authorSpreadInfo = await fetch(`https://www.xingtu.cn/gw/api/data_sp/get_author_spread_info?o_author_id=${id}` +
+        "&platform_source=1&platform_channel=1&range=2&type=2&only_assign=true", {
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+    });
+    let authorShowItems = await fetch(`https://www.xingtu.cn/gw/api/author/get_author_show_items_v2?o_author_id=${id}` +
+        "&platform_source=1&platform_channel=1&limit=10&only_assign=true", {
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+    });
+    return [await authorSpreadInfo.json(), await authorShowItems.json()]
+}
