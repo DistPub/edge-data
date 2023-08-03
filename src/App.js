261317131af3514f4f8eb7be40ede1d088dce872
Smite Chow <xiaopengyou2no1@gmail.com>
2023-08-03 21:52:02 +0800

    tidy

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit the <code>src/App.js</code> and save to reload.
+          I <span className="App-link">❤</span> edge-data.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Smite from "./smite";
 import Xingtu from "./xingtu/App";
 import Xiaohongshu from "./xiaohongshu/App";
 
@@ -13,10 +11,6 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <App/>,
-    },
-    {
-        path: "/smite",
-        element: <Smite/>,
     },
     {
         path: "/xingtu",
@@ -34,8 +28,3 @@ root.render(
         <RouterProvider router={router} />
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
